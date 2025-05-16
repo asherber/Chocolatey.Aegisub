c8d30d2110972b9b315ef3c3009006c498eda2f5
Aaron Sherber <aaron@sherber.com>
2025-05-15 22:49:08 -0400

    v3.4.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
    packageName = 'aegisub'
    fileType = 'exe'
    silentArgs = '/verysilent'
-   url64bit = 'https://github.com/TypesettingTools/Aegisub/releases/download/v3.4.1/Aegisub-3.4.1.exe'
-   checksum64 = '8b95a0194f1a74837aac4c7d0b471098'
+   url64bit = 'https://github.com/TypesettingTools/Aegisub/releases/download/v3.4.2/Aegisub-3.4.2.exe'
+   checksum64 = '609CCAFF7804453416966E6BBCC90ED0'
    checksumType64 = 'md5'
    
 }
